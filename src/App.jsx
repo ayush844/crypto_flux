2a6303258e7c72c394a467f7f9e72d7bd115c6f5
ayush844 <ayush.xyz1625@gmail.com>
2024-09-06 22:05:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="text-3xl font-bold underline text-red-700">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
